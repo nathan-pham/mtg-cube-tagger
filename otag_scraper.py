8de97a1d9c95d8bbeea6e1567cd4ece3aa207bf2
nathan-pham <157486459+nathan-pham@users.noreply.github.com>
2024-12-29 12:54:40 +1100

    Test tag filtering

--- otag_scraper.py
+++ otag_scraper.py
@@ -5,10 +5,11 @@ import requests
 def find_tags(set_code, collector_number):
     print(f"https://tagger.scryfall.com/card/{set_code}/{collector_number}")
     html_text = requests.get(f"https://tagger.scryfall.com/card/{set_code}/{collector_number}").text
-    print(html_text)
     soup = BeautifulSoup(html_text, 'lxml')
     tags = soup.find_all('div', class_= 'tag-row')
+
     o_tags = []
+
     for tag in tags:
         o_tags.append(tag.text)
         print(tag.text)
