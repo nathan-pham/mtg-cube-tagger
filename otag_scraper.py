6b701ce75a579b627baf6d4c556a0c0c54d7c116
nathan-pham <157486459+nathan-pham@users.noreply.github.com>
2024-12-29 13:36:59 +1100

    Implement tag scraping

--- otag_scraper.py
+++ otag_scraper.py
@@ -1,18 +1,33 @@
 import scrython
 from bs4 import BeautifulSoup
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+chromedriver_path = '/usr/bin/chromedriver'
+service = Service(chromedriver_path)
+
 
 def find_tags(set_code, collector_number):
     print(f"https://tagger.scryfall.com/card/{set_code}/{collector_number}")
-    html_text = requests.get(f"https://tagger.scryfall.com/card/{set_code}/{collector_number}").text
-    soup = BeautifulSoup(html_text, 'lxml')
-    tags = soup.find_all('div', class_= 'tag-row')
 
+    driver = webdriver.Chrome(service=service)
+    driver.get(f"https://tagger.scryfall.com/card/{set_code}/{collector_number}")
+
+    for _ in range(100):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    tags = driver.find_elements(By.CLASS_NAME, "tag-row")
     o_tags = []
 
     for tag in tags:
-        o_tags.append(tag.text)
-        print(tag.text)
+        tag_text = tag.text
+        if 'Annotation' not in tag_text:
+            o_tags.append(tag.text)
+
+    for tag in o_tags:
+        print(tag)
 
 print("Search Magic: The Gathering card")
 query = input('> ')
