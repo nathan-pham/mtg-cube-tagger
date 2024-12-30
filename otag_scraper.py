edcd3957a9662e896920f18e8cca555fa3b69687
nathan-pham <157486459+nathan-pham@users.noreply.github.com>
2024-12-30 11:57:44 +1100

    Prototype import CSV

--- otag_scraper.py
+++ otag_scraper.py
@@ -3,32 +3,31 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import ChromiumOptions
+import csv
 
 # Set up ChromeDriver settings
 chromedriver_path = '/usr/bin/chromedriver'
 service = Service(chromedriver_path)
 options = ChromiumOptions()
 options.add_argument("--headless=new")
-
+options.add_argument('--blink-settings=imagesEnabled=false')
 
 class Card:
-    def __init__(self, name):
+    def __init__(self, name, mode):
         self.scryfall = scrython.cards.Named(fuzzy=name)
         self.name = self.scryfall.name()
         self.link = self.find_link()
-        self.tags = self.find_tags(self.link)
+        self.tags = self.find_tags(self.link, mode)
+        self.mode = mode
 
-    def find_tags(self, link):
-
-        print("Which tags would you like to search for? (all/art/card)")
-        mode = input('> ')
+    def find_tags(self, link, mode):
 
         # Initialise driver
         driver = webdriver.Chrome(options=options, service=service)
         driver.get(link)
 
         # Scroll down page
-        for _ in range(100):
+        for _ in range(50):
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
         # Find elements based on mode selected
@@ -45,10 +44,13 @@ class Card:
         # Convert tags to strings and append to new list
         o_tags = []
         for tag in tags:
-            tag_text = tag.text
             o_tags.append(tag.text)
 
         driver.quit()
+
+        # Verbose debuggging
+        print(self.name)
+        print(o_tags)
 
         return o_tags
 
@@ -57,10 +59,35 @@ class Card:
         collector_number = self.scryfall.collector_number()
         return(f"https://tagger.scryfall.com/card/{set_code}/{collector_number}")
 
+class cardList:
+    def __init__(self, csvPath):
+        cardList.csvPath = csvPath
+        cardList.cards = cardList.import_cards(self)
+        cardList.namePos = int
+        cardList.tagsPos = int
+    
+    def import_cards(self):
+
+        cards = []
+
+        with open(self.csvPath, newline='') as csvfile:
+            reader = csv.reader(csvfile)
+
+            # Find which columns are the name and tag columns
+            firstRow = next(reader)
+            for i in range(len(firstRow)):
+                if (firstRow)[i] == 'name':
+                    self.namePos = i
+                elif (firstRow)[i] == 'tags':
+                    self.tagsPos = i
+
+            for i in reader:
+                print("Loading...")
+                cards.append(Card(i[self.namePos], "all"))
+    
+        return cards
+
 if __name__ == "__main__":
-    print("What Magic: The Gathering card would you like the tags for?")
-    card_name = input("> ")
-    card1 = Card(card_name)
-    print(card1.name)
-    print(card1.link)
-    print(card1.tags)
+    cardList('example.csv')
+    for i in cardList.cards:
+        print(i.tags)
