35186f16e393989f0ee1f22fff06a8dd23e9df9a
nathan-pham <157486459+nathan-pham@users.noreply.github.com>
2025-01-01 14:11:46 +1100

    Change tag fetching from scraping to API call

--- otag_scraper.py
+++ otag_scraper.py
@@ -4,65 +4,25 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import ChromiumOptions
 import csv
-
-# Set up ChromeDriver settings
-chromedriver_path = '/usr/bin/chromedriver'
-service = Service(chromedriver_path)
-options = ChromiumOptions()
-options.add_argument("--headless=new")
-options.add_argument('--blink-settings=imagesEnabled=false')
+from urllib.request import urlopen
+import json
 
 class Card:
     def __init__(self, name, mode):
         self.scryfall = scrython.cards.Named(exact=name)
         self.name = self.scryfall.name()
-        self.link = self.find_link()
-        self.tags = self.find_tags(self.link, mode)
+        self.oracleID = self.scryfall.oracle_id()
+        self.tags = self.find_tags()
         self.mode = mode
 
-    def find_tags(self, link, mode):
+    def find_tags(self):
+        oracle_tags = []
 
-        # Initialise driver
-        driver = webdriver.Chrome(options=options, service=service)
-        driver.get(link)
+        for tag in data_json['data']:
+            if self.oracleID in tag['oracle_ids']:
+                oracle_tags.append(tag['label'].strip('"'))
 
-        o_tags = []
-
-        i = 0
-        while not o_tags and i < 10:
-
-            print(f"Searching {self.name}...")
-
-            # Scroll down page
-            for _ in range(100):
-                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-            # Find elements based on mode selected
-            if mode == 'all':
-                tags = driver.find_elements(By.XPATH, "//a[contains(@href, '/tags/')]")
-            elif mode == 'art':
-                tags = driver.find_elements(By.XPATH, "//a[contains(@href, '/tags/artwork/')]")
-            elif mode == 'card':
-                tags = driver.find_elements(By.XPATH, "//a[contains(@href, '/tags/card/')]")
-
-            # Convert tags to strings and append to new list
-            for tag in tags:
-                o_tags.append(tag.text.strip('"'))
-
-            i += 1
-
-        driver.quit()
-
-        # Verbose debuggging
-        print(self.name)
-        print(o_tags)
-
-        return o_tags
-
-    def find_link(self):
-        set_code = self.scryfall.set_code()
-        collector_number = self.scryfall.collector_number()
-        return(f"https://tagger.scryfall.com/card/{set_code}/{collector_number}")
+        return oracle_tags
 
 class cardList:
     def __init__(self, csvPath):
